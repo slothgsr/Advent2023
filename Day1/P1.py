b8cc0f7037791ebc96a43bc20fc98b35d6280c34
slothgsr <dustin.rego@gmail.com>
2023-12-05 16:10:47 -0500

    DAY_P1 finished

--- Day1/P1.py
+++ Day1/P1.py
@@ -1,2 +1,40 @@
-#test
+import os
+import sys
+
+abspath = os.path.abspath(sys.argv[0])
+dname = os.path.dirname(abspath)
+file = (r'Day1\P1.txt')
+
+
+def findnumbers(arg1):
+    FN = None   
+    LN = None
+    for i in arg1:
+        try:
+            i == int(i)
+            if i.isdigit:
+                FN = i
+            break
+        except:
+            continue
+    for i in arg1[::-1]:
+        try:
+            i == int(i)
+            if i.isdigit:
+                LN = i
+            break
+        except:
+            continue
+    return int(FN+LN)
+        
+
+with open(file) as f:
+    lines = [line.rstrip('\n') for line in f]
+
+numberlist = []
+for i in lines:
+    numberlist.append(findnumbers(i))
+print(sum(numberlist))
+
+
 
